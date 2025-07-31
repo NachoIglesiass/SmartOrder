0573011861cad13b37a8b2cc975650d6360842c6
NachoIglesiass <nachoiglesias36@gmail.com>
2025-07-31 14:48:25 -0300

    Add files via upload

---
+++ smartorder-frontend/next-frontend/src/utils/api.js
@@ -0,0 +1,43 @@
+// src/utils/api.js
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const api = {
+  get: (endpoint, options = {}) =>
+    fetch(`${API_URL}${endpoint}`, {
+      method: 'GET',
+      credentials: 'include',
+      ...options,
+    }),
+
+  post: (endpoint, body, options = {}) =>
+    fetch(`${API_URL}${endpoint}`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        ...(options.headers || {}),
+      },
+      body: JSON.stringify(body),
+      ...options,
+    }),
+
+  put: (endpoint, body, options = {}) =>
+    fetch(`${API_URL}${endpoint}`, {
+      method: 'PUT',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        ...(options.headers || {}),
+      },
+      body: JSON.stringify(body),
+      ...options,
+    }),
+
+  delete: (endpoint, options = {}) =>
+    fetch(`${API_URL}${endpoint}`, {
+      method: 'DELETE',
+      credentials: 'include',
+      ...options,
+    }),
+};
