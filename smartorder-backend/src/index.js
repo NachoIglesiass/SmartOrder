0c42dcccce0808aa41cc386c16a1feed32dbac0d
NachoIglesiass <nachoiglesias36@gmail.com>
2025-07-31 22:47:03 -0300

    Update index.js

--- smartorder-backend/src/index.js
+++ smartorder-backend/src/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 const allowedOrigins = [
   'http://localhost:3000',
   'https://smartorder-frontend.vercel.app',
-  'https://smartorder-frontend-jjy7ozmus-smartorders-projects.vercel.app'
+  'https://smartorder-frontend-byvrz39mt-smartorders-projects.vercel.app'
 ];
 
 app.use(cors({
